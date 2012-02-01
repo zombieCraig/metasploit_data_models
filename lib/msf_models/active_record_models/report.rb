222f0dacdb2bfa1eeafcd83050a8705bb98faa14
Brian Gamble <Brian_Gamble@rapid7.com>
2012-01-31 18:41:22 -0600

    Msm::Report - move validation, callback, and scope from pro ui

--- lib/msf_models/active_record_models/report.rb
+++ lib/msf_models/active_record_models/report.rb
@@ -1,10 +1,23 @@
 module MsfModels::ActiveRecordModels::Report
   def self.included(base)
-    base.class_eval{
+    base.class_eval {
       include Msf::DBManager::DBSave
 
       belongs_to :workspace, :class_name => "Msm::Workspace"
       serialize :options, ::MsfModels::Base64Serializer.new
+
+      validates_format_of :name, :with => /^[A-Za-z0-9\x20\x2e\x2d\x5c]+$/, :message => "name must be A-Z, 0-9, space, dot, underscore, or dash", :allow_blank => true
+
+      scope :flagged, where('reports.downloaded_at is NULL')
+
+      before_destroy :delete_file
+
+      private
+
+      def delete_file
+        c = Pro::Client.get
+        c.report_delete_file(self[:id])
+      end
     }
   end
 end
